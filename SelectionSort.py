b815c341f2c9b6445712b0fa34dad0db1c5d564b
AHSAN HABIB <143648731+ahsanauddry027@users.noreply.github.com>
2023-11-27 21:49:11 +0600

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -4,18 +4,16 @@ import numpy as np
 arr = np.array([5,6,3,8,2])
 
 for i in range(len(arr)-1):
-
     min= arr[i]
     idx = 0
+    
     for j in range(i+1,len(arr)):
-      
-        
         if arr[j]<min:
             min = arr[j]
             idx = j
+            
     arr[i],arr[idx]=min,arr[i]
 print(arr)
-
 
 #Sorting With Recursion
 def selectionSort(arr):
@@ -26,26 +24,19 @@ def selectionSort(arr):
         if arr[j]<min:
             min = arr[j]
             idx = j
-       
         return subFun(min,j+1,idx)
-        
-            
         
     def mainFun(i=0):
         min = arr[i]    
         if i==len(arr)-1:
             return 0
-
+            
         main =subFun(min,i+1)
         arr[i],arr[main[1]]=main[0],arr[i]
         mainFun(i+1)
         
     mainFun(0)
     return arr
-        
-    
-        
-    
     
 arr1 = [5,6,3,8,2]
 print(selectionSort(arr1))
